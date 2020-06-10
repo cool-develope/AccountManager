7026e605482c59c53796be62125dc298bc3104ef
cool199966 <cool199966@outlook.com>
2020-06-10 13:27:18 +0800

    Update setting

--- AccountManager/settings.py
+++ AccountManager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k=nc2j9u0g4ryqtldyz4iwuom8cxlg4(lhq$p)4jz6y=i(6-h$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '192.168.0.111', '78.141.194.252']
 
--- Auths/admin.py
+++ Auths/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.models import Permission
 from .models import MyUser
 # Register your models here.
+
 admin.site.register(MyUser)
 admin.site.register(Permission)
